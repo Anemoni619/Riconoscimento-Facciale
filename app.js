09556ea7ada55f852956dd7155d0ea9a3b4f3c53
Michele Barbagallo <50728628+Anemoni619@users.noreply.github.com>
2019-11-07 21:42:18 +0100

    verificato video 7_11_2019
    funziona il riconoscimento continuo del video

--- app.js
+++ app.js
@@ -5,21 +5,22 @@ const MODEL_URL = "data/face-api.js/weights"
 
 const loader = document.getElementById("loader")
 const webcam = document.getElementById("webcam")
-const webcamSize = {width: 1280, height:720}
+//const webcamSize = {width: 1280, height:720}
+const webcamSize = {width: 320, height:320}
 
 webcam.addEventListener("play", async() => {
     const faceOverlay = faceapi.createCanvasFromMedia(webcam)
     document.body.append(faceOverlay)
     faceOverlay.style = 'z-index: 1; position: absolute; top: ' + webcam.offsetTop + ';left: ' + webcam.offsetLeft + ';'
     faceapi.matchDimensions(faceOverlay, webcamSize)
-
-    const facesDetected = await faceapi.detectAllFaces(webcam).withFaceLandmarks().withFaceDescriptors()
-    console.log(facesDetected)
-    const resizedFaces = faceapi.resizeResults(facesDetected , webcamSize)
-    faceOverlay.getContext('2d').clearRect(0, 0, faceOverlay.width, faceOverlay.height)
-    //console.log(faceOverlay, resizedFaces)
-    faceapi.draw.drawDetections(faceOverlay, resizedFaces)
-    faceapi.draw.drawFaceLandmarks(faceOverlay, resizedFaces)
+    setInterval ( async() => {
+        const facesDetected = await faceapi.detectAllFaces(webcam).withFaceLandmarks().withFaceDescriptors()
+        const resizedFaces = faceapi.resizeResults(facesDetected , webcamSize)
+        faceOverlay.getContext('2d').clearRect(0, 0, faceOverlay.width, faceOverlay.height)
+        //console.log(faceOverlay, resizedFaces)
+        faceapi.draw.drawDetections(faceOverlay, resizedFaces)
+        faceapi.draw.drawFaceLandmarks(faceOverlay, resizedFaces)
+    },100)
 })
 
 
